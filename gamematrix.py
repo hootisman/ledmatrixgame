daf6dd2fc4bd540045e1c98a3f8dcaec9675b331
hootisman <david1zalewski@gmail.com>
2024-05-06 17:36:33 -0400

    snake class

--- gamematrix.py
+++ gamematrix.py
@@ -9,36 +9,41 @@ from rgbmatrix import graphics
 from enum import Enum
 
 running = True
-x_pos = 32
-y_pos = 32
 
-def user_input():
-    global running
-    global x_pos
-    global y_pos
+class Snake(object):
+    class Direction(Enum):
+        UP = 1
+        DOWN = 2
+        LEFT = 3
+        RIGHT = 4
 
-    while running:
-        key = readchar.readkey()
-        match key:
-            case 'a':
-                x_pos = x_pos - 1
-            case 'd':
-                x_pos = x_pos + 1
-            case 'w':
-                y_pos = y_pos - 1
-            case 's':
-                y_pos = y_pos + 1
-            case 'q' | 0x1B | 0x04:
-                running = False
-            case _:
-                pass
-        print(key)
+    def __init__(self):
+        self.x_pos = 32
+        self.y_pos = 32
 
-class Direction(Enum):
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT = 4
+        self.input_t = Thread(target=self.user_input)
+        self.input_t.start()
+
+    def user_input(self):
+        global running
+
+        while running:
+            key = readchar.readkey()
+            match key:
+                case 'a':
+                    self.x_pos = self.x_pos - 1
+                case 'd':
+                    self.x_pos = self.x_pos + 1
+                case 'w':
+                    self.y_pos = self.y_pos - 1
+                case 's':
+                    self.y_pos = self.y_pos + 1
+                case 'q' | 0x1B | 0x04:
+                    running = False
+                case _:
+                    pass
+            print(key)
+
 
 class LEDGame(MatrixBase):
     def __init__(self, *args, **kwargs):
@@ -46,27 +51,27 @@ class LEDGame(MatrixBase):
     
     def run(self):
         global running
-        global x_pos
-        global y_pos
+        
+        snake = Snake()
 
         canvas = self.matrix.CreateFrameCanvas()
 
         paccol = graphics.Color(255,255,0)
         while running:
             self.matrix.Clear()
-            #graphics.DrawCircle(self.matrix, x_pos, y_pos, 2, paccol)
-            graphics.DrawLine(self.matrix, x_pos, y_pos -2, x_pos, y_pos, paccol)
+            #graphics.DrawCircle(self.matrix, snake.x_pos, snake.y_pos, 2, paccol)
+            graphics.DrawLine(self.matrix, snake.x_pos, snake.y_pos -2, snake.x_pos, snake.y_pos, paccol)
 
 
-            y_pos = -6 if y_pos >= 68 else y_pos + 0.05
+            snake.y_pos = -6 if snake.y_pos >= 68 else snake.y_pos + 0.05
 
 
             canvas = self.matrix.SwapOnVSync(canvas)
             
 # Main function
 if __name__ == "__main__":
-    input_t = Thread(target=user_input)
-    input_t.start()
+    #input_t = Thread(target=user_input)
+    #input_t.start()
 
     simple_square = LEDGame()
     if (not simple_square.process()):
